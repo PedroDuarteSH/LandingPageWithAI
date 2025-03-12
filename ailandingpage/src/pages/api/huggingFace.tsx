3f9a0cde6c13648c9ae9a25d4e3600c00b59b5db
Pedro Henriques <pedroduartesh@gmail.com>
2025-03-12 22:37:24 +0000

    Updates

--- ailandingpage/src/pages/api/huggingFace.tsx
+++ ailandingpage/src/pages/api/huggingFace.tsx
@@ -22,8 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 const newContent = chunk.choices[0].delta.content;
                 console.log(newContent)
                 res.write(newContent);
-               
-               
+                
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 //(res as any).flush();
             }
